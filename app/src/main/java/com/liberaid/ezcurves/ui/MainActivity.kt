1d02d6374acd35f4dd9db96bce1e509ab1583b29
Egor Shitov <shitov.egor2@mail.ru>
2019-11-04 23:11:21 +0300

    Edit image in EditFragment

--- app/src/main/java/com/liberaid/ezcurves/ui/MainActivity.kt
+++ app/src/main/java/com/liberaid/ezcurves/ui/MainActivity.kt
@@ -1,94 +1,12 @@
 package com.liberaid.ezcurves.ui
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.Allocation
-import android.renderscript.Element
-import android.renderscript.RenderScript
-import com.liberaid.ezcurves.ui.custom.CurveView
+import androidx.appcompat.app.AppCompatActivity
 import com.liberaid.ezcurves.R
-import com.liberaid.ezcurves.util.withUI
-import com.liberaid.renderscripttest.ScriptC_curve
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import timber.log.Timber
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var bitmap: Bitmap
-    private lateinit var rs: RenderScript
-    private lateinit var inAlloc: Allocation
-    private lateinit var outAlloc: Allocation
-    private lateinit var redCurveAlloc: Allocation
-    private lateinit var script: ScriptC_curve
-
-    private val notifyChannel = Channel<Unit>(Channel.CONFLATED)
-    private var handlerJob: Job? = null
-
-    private val redCurve = ByteArray(256)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    private fun setup() {
-        bitmap = try {
-            BitmapFactory.decodeStream(assets.open("me_and_wall.jpg"))
-        } catch (e: Exception) {
-            Timber.d("Cannot open bitmap")
-            e.printStackTrace()
-            return
-        }
-
-        rs = RenderScript.create(this)
-
-        inAlloc = Allocation.createFromBitmap(rs, bitmap)
-        outAlloc = Allocation.createTyped(rs, inAlloc.type)
-
-        redCurveAlloc = Allocation.createSized(rs, Element.U8(rs), 256)
-
-        script = ScriptC_curve(rs).apply {
-            bind_mapping_r(redCurveAlloc)
-            bind_mapping_g(redCurveAlloc)
-            bind_mapping_b(redCurveAlloc)
-        }
-
-        curveView.curveChangedListener = object :
-            CurveView.ICurveChangedListener {
-            override fun onCurveChanged() {
-                runBlocking {
-                    notifyChannel.send(Unit)
-                }
-            }
-        }
-
-        handlerJob?.cancel()
-        handlerJob = GlobalScope.launch { handleNotifyChannel() }
-    }
-
-    private suspend fun handleNotifyChannel() = coroutineScope {
-        for(notify in notifyChannel){
-            curveView.fillCurve(redCurve)
-
-            redCurveAlloc.copy1DRangeFrom(0, 256, redCurve)
-
-            script.forEach_apply(inAlloc, outAlloc)
-            outAlloc.copyTo(bitmap)
-
-            withUI {
-                ivPreview.setImageBitmap(bitmap)
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        handlerJob?.cancel()
-        handlerJob = null
-        super.onDestroy()
     }
 }
--- app/src/main/java/com/liberaid/ezcurves/ui/fragments/editfragment/EditFragment.kt
+++ app/src/main/java/com/liberaid/ezcurves/ui/fragments/editfragment/EditFragment.kt
@@ -1,27 +1,115 @@
 package com.liberaid.ezcurves.ui.fragments.editfragment
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.navigation.Navigation
+import android.renderscript.Allocation
+import android.renderscript.Element
+import android.renderscript.RenderScript
 import com.bumptech.glide.Glide
-import com.liberaid.ezcurves.R
+import com.bumptech.glide.request.FutureTarget
+import com.liberaid.ezcurves.MyApp
 import com.liberaid.ezcurves.ui.BaseFragment
 import com.liberaid.ezcurves.ui.FragmentId
+import com.liberaid.ezcurves.ui.custom.CurveView
 import com.liberaid.ezcurves.ui.fragments.selectfragment.SelectFragment
+import com.liberaid.ezcurves.util.withUI
+import com.liberaid.renderscripttest.ScriptC_curve
 import kotlinx.android.synthetic.main.fragment_edit.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import timber.log.Timber
 
 class EditFragment : BaseFragment() {
     override val fragmentId = FragmentId.EDIT_FRAGMENT
 
+    private var bitmap: Bitmap? = null
+    private var rs: RenderScript? = null
+    private var inAlloc: Allocation? = null
+    private var outAlloc: Allocation? = null
+    private var script: ScriptC_curve? = null
+    private var redCurveAllocation: Allocation? = null
+    private var greenCurveAllocation: Allocation? = null
+    private var blueCurveAllocation: Allocation? = null
+
+    private val curveBuffer = ByteArray(256)
+    private val notifyChannel = Channel<Unit>(Channel.CONFLATED)
+
+    private var handlerJob: Job? = null
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        val appComponent = (activity?.application as? MyApp?)?.appComponent
+        if(appComponent == null){
+            Timber.w("App component is null")
+            return
+        }
+
+        rs = appComponent.getRenderScript()
+        redCurveAllocation = Allocation.createSized(rs, Element.U8(rs), 256)
+        greenCurveAllocation = Allocation.createSized(rs, Element.U8(rs), 256)
+        blueCurveAllocation = Allocation.createSized(rs, Element.U8(rs), 256)
+
+        script = appComponent.getScriptCurve().apply {
+            bind_mapping_r(redCurveAllocation)
+            bind_mapping_g(greenCurveAllocation)
+            bind_mapping_b(blueCurveAllocation)
+        }
+
         val imagePath = arguments?.getString(SelectFragment.IMAGE_PATH_KEY) ?: return
 
-        Glide.with(this)
+        val imageFuture = Glide.with(this)
+            .asBitmap()
             .load(imagePath)
-            .centerCrop()
-            .into(ivPreview)
+            .submit()
 
-        btnExport.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_editFragment_to_importFragment))
+        handlerJob?.cancel()
+        handlerJob = setupByFuture(imageFuture)
+    }
+
+    private fun setupByFuture(imageFuture: FutureTarget<Bitmap>) = GlobalScope.launch {
+        bitmap = imageFuture.get()
+        withUI {
+            ivPreview.setImageBitmap(bitmap)
+        }
+
+        inAlloc = Allocation.createFromBitmap(rs, bitmap)
+        outAlloc = Allocation.createTyped(rs, inAlloc!!.type)
+
+        launch { handleNotifyChannel() }
+
+        withUI {
+            curveView.curveChangedListener = object : CurveView.ICurveChangedListener {
+                override fun onCurveChanged() {
+                    runBlocking {
+                        notifyChannel.send(Unit)
+                    }
+                }
+            }
+        }
+    }
+
+    private suspend fun handleNotifyChannel() = coroutineScope {
+        for(notify in notifyChannel){
+            curveView.fillCurve(curveBuffer)
+
+            redCurveAllocation?.copy1DRangeFrom(0, 256, curveBuffer)
+            greenCurveAllocation?.copy1DRangeFrom(0, 256, curveBuffer)
+            blueCurveAllocation?.copy1DRangeFrom(0, 256, curveBuffer)
+
+            script?.forEach_apply(inAlloc, outAlloc)
+            outAlloc?.copyTo(bitmap)
+
+            withUI {
+                ivPreview.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        handlerJob?.cancel()
+        handlerJob = null
+
+        super.onDestroyView()
     }
 }
